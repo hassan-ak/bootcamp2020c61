a1902553e4e2a510f2dd0f675b3e4241d167e74b
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-30 04:05:17 +0500

    Update example00_create_auto_secret-stack.ts

--- example00_create_auto_secret/lib/example00_create_auto_secret-stack.ts
+++ example00_create_auto_secret/lib/example00_create_auto_secret-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class Example00CreateAutoSecretStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -23,5 +25,38 @@ export class Example00CreateAutoSecretStack extends cdk.Stack {
         generateStringKey: "SecretKey",
       },
     });
+
+    // Lambda Function
+    const lambdafn = new lambda.Function(this, "Step13-1LambdaFunction", {
+      functionName: "Step13-1LambdaFunction",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+      environment: {
+        SECRET_KEY1: `${
+          secretsmanager.Secret.fromSecretAttributes(this, "SecretKey1", {
+            secretCompleteArn: secret1.secretArn,
+          }).secretValue
+        }`,
+        SECRET_KEY2: `${
+          secretsmanager.Secret.fromSecretNameV2(
+            this,
+            "SecretKey2",
+            secret2.secretName
+          ).secretValue
+        }`,
+      },
+    });
+
+    // Role access for lambda function
+    lambdafn.grantInvoke(
+      new iam.ServicePrincipal("secretsmanager.amazonaws.com")
+    );
+    lambdafn.addToRolePolicy(
+      new iam.PolicyStatement({
+        resources: [secret3.secretArn],
+        actions: ["secretsmanager:GetSecretValue"],
+      })
+    );
   }
 }
