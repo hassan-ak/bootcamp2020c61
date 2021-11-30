55aa64f2f6f00f41134e7916fb4849f4693e3394
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-30 14:59:07 +0500

    Update example01_rotate_secret_with_lambda-stack.ts

--- example01_rotate_secret_with_lambda/lib/example01_rotate_secret_with_lambda-stack.ts
+++ example01_rotate_secret_with_lambda/lib/example01_rotate_secret_with_lambda-stack.ts
@@ -1,15 +1,53 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class Example01RotateSecretWithLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create new secret
+    const secret = new secretsmanager.Secret(this, "Secret", {
+      secretName: "secret-rotating",
+      description: "Rotating Secret",
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: "SecretKey",
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Example01RotateSecretWithLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda Function
+    const lambdafn = new lambda.Function(this, "lambdaSecretRotate", {
+      functionName: "lambdaSecretRotate",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+      environment: {
+        REGION: cdk.Stack.of(this).region,
+        SECRET_NAME: "secret-rotating",
+        SECRET_KEY: "SecretKey",
+      },
+    });
+
+    // Add rotation to secret
+    secret.addRotationSchedule("RotationSchedule", {
+      rotationLambda: lambdafn,
+      automaticallyAfter: cdk.Duration.hours(24),
+    });
+
+    // Add read access to secret
+    secret.grantRead(lambdafn);
+
+    // define role for lambda function
+    lambdafn.grantInvoke(
+      new iam.ServicePrincipal("secretsmanager.amazonaws.com")
+    );
+    lambdafn.addToRolePolicy(
+      new iam.PolicyStatement({
+        resources: [secret.secretArn],
+        actions: ["secretsmanager:PutSecretValue"],
+      })
+    );
   }
 }
