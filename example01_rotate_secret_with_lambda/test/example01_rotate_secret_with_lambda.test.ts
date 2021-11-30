d0135a7aac744fbb150a066d19bbf93d31ac0188
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-11-30 13:42:30 +0500

    Create cdk app

---
+++ example01_rotate_secret_with_lambda/test/example01_rotate_secret_with_lambda.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Example01RotateSecretWithLambda from '../lib/example01_rotate_secret_with_lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example01_rotate_secret_with_lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Example01RotateSecretWithLambda.Example01RotateSecretWithLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
